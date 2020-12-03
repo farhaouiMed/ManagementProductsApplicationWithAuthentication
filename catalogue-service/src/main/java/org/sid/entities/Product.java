157955cb97f34e2e2695388e3a21cf9291ebb5f8
farhaouiMed <mohamed.farhaoui@etudiant-enit.utm.tn>
2020-12-03 23:36:09 +0100

    adding management products microservice

---
+++ catalogue-service/src/main/java/org/sid/entities/Product.java
@@ -0,0 +1,24 @@
+package org.sid.entities;
+
+import java.util.Collection;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Document
+@Data @AllArgsConstructor @NoArgsConstructor 
+public class Product {
+	@Id
+	private String id;
+	private String name;
+	private double price;
+	@DBRef
+	private Category category;
+	
+}	
