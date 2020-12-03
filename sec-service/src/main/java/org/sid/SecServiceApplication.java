959795d02b56cca65e5fcf64d60411e6a1c6c373
farhaouiMed <mohamed.farhaoui@etudiant-enit.utm.tn>
2020-12-03 23:34:30 +0100

    adding securtity microservice

---
+++ sec-service/src/main/java/org/sid/SecServiceApplication.java
@@ -0,0 +1,36 @@
+package org.sid;
+
+import java.util.stream.Stream;
+
+import org.sid.entities.AppRole;
+import org.sid.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class SecServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecServiceApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner start(AccountService accountService) {
+		return args->{
+			accountService.saveRole(new AppRole(null, "USER"));
+			accountService.saveRole(new AppRole(null, "ADMIN"));
+			Stream.of("user1","user2","user3","admin").forEach(u->{
+				accountService.saveUser(u, "1234", "1234");
+			});
+			accountService.addRoleToUser("admin", "ADMIN");
+		};
+	}
+	@Bean
+	BCryptPasswordEncoder getBCPE() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
